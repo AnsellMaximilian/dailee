c6ffcd02b43f71681bbf079678df9f7326c4e596
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-07-04 19:11:04 +0700

    double column for form and list on wide screen

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,140 +1,150 @@
-import React, { useState } from 'react'
-import { firestore } from '../firebase/config';
-import firebase from 'firebase';
-import { Button, Collapse, Container, FormControlLabel, Grid, makeStyles, Radio, RadioGroup, TextField, Typography } from '@material-ui/core';
+import React, { useState } from "react";
+import { firestore } from "../firebase/config";
+import firebase from "firebase";
+import {
+  Button,
+  Container,
+  FormControlLabel,
+  Grid,
+  makeStyles,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@material-ui/core";
 // import { ExpandMore } from '@material-ui/icons';
 
 interface Props {
-    user: firebase.User
+  user: firebase.User;
 }
 
-const useStyles = makeStyles(theme => ({
-    toggleButton: {
-        backgroundColor: theme.palette.grey[50],
-        borderRadius: 0
-    },
+const useStyles = makeStyles((theme) => ({
+  toggleButton: {
+    backgroundColor: theme.palette.grey[50],
+    borderRadius: 0,
+  },
 
-    formContainer: {
-        margin: theme.spacing(2, 'auto')
-    },
+  formContainer: {
+    margin: theme.spacing(2, "auto"),
+  },
 
-    formMode: {
-        display: 'flex',
-        justifyContent: 'start',
-        flexDirection: 'row'
-    }
-}))
+  formMode: {
+    display: "flex",
+    justifyContent: "start",
+    flexDirection: "row",
+  },
+}));
 
-export default function TaskForm({user}: Props) {
-    const classes = useStyles();
+export default function TaskForm({ user }: Props) {
+  const classes = useStyles();
 
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [timeFrame, setTimeFrame] = useState('');
-    const [isFormOpen, setIsFormOpen] = useState(false);
-    const [formMode, setformMode] = useState<'tasks' | 'reserveTasks'>('tasks');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [timeFrame, setTimeFrame] = useState("");
+  const [formMode, setformMode] = useState<"tasks" | "reserveTasks">("tasks");
 
-    const handleChange: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement> = (e) => {
-        let value = e.target.value
+  const handleChange: React.ChangeEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  > = (e) => {
+    let value = e.target.value;
 
-        if(e.target.name === 'title') setTitle(value)
-        else if(e.target.name === 'description') setDescription(value)
-        else if(e.target.name === 'time-frame') setTimeFrame(value)
-    }
+    if (e.target.name === "title") setTitle(value);
+    else if (e.target.name === "description") setDescription(value);
+    else if (e.target.name === "time-frame") setTimeFrame(value);
+  };
 
-    const handleFormModeChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        let value: 'tasks' | 'reserveTasks' = e.target.value === 'reserveTasks' ? 'reserveTasks' : 'tasks';
-        setformMode(value); 
+  const handleFormModeChange: React.ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    let value: "tasks" | "reserveTasks" =
+      e.target.value === "reserveTasks" ? "reserveTasks" : "tasks";
+    setformMode(value);
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    let task = {
+      user: user.uid,
+      title,
+      description,
+      timeFrame,
     };
 
-    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault();
+    firestore.collection(formMode).add(task).then().catch();
 
-        let task = { 
-            user: user.uid,
-            title,
-            description,
-            timeFrame
-        }
+    setTitle("");
+    setDescription("");
+    setTimeFrame("");
+  };
 
-        firestore.collection(formMode).add(task)
-            .then()
-            .catch();
-        
-        setTitle('');
-        setDescription('');
-        setTimeFrame('')
-    }
-
-    return (
-        <div>
-            <Button 
+  return (
+    <div>
+      <Container maxWidth="xs" className={classes.formContainer}>
+        <RadioGroup
+          aria-label="formMode"
+          name="form-mode"
+          value={formMode}
+          onChange={handleFormModeChange}
+          className={classes.formMode}
+        >
+          <FormControlLabel value="tasks" control={<Radio />} label="Tasks" />
+          <FormControlLabel
+            value="reserveTasks"
+            control={<Radio />}
+            label="Reserve Tasks"
+          />
+        </RadioGroup>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={1}>
+            <Grid item xs={8}>
+              <TextField
+                required
                 fullWidth
+                variant="outlined"
+                type="text"
+                name="title"
+                label="Task Title"
+                onChange={handleChange}
+                value={title}
+              />
+            </Grid>
+
+            <Grid item xs={4}>
+              <TextField
+                fullWidth
+                variant="outlined"
+                type="time"
+                name="time-frame"
+                onChange={handleChange}
+                value={timeFrame}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                variant="outlined"
+                multiline
+                name="description"
+                placeholder="Task Description"
+                onChange={handleChange}
+                value={description}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                type="submit"
                 variant="contained"
-                onClick={() => setIsFormOpen(state => !state)}
-                className={classes.toggleButton}
-            >
-                <Typography>Create Task</Typography>
-            </Button>
-            <Collapse in={isFormOpen}>
-                <Container maxWidth="xs" className={classes.formContainer}>
-                    <RadioGroup 
-                        aria-label="formMode" 
-                        name="form-mode" 
-                        value={formMode} 
-                        onChange={handleFormModeChange}
-                        className={classes.formMode}
-                    >
-                        <FormControlLabel value="tasks" control={<Radio />} label="Tasks" />
-                        <FormControlLabel value="reserveTasks" control={<Radio />} label="Reserve Tasks" />
-                    </RadioGroup>
-                    <form onSubmit={handleSubmit}>
-                        <Grid container spacing={1}>
-                            <Grid item xs={8}>
-                                <TextField
-                                    required
-                                    fullWidth 
-                                    variant="outlined" 
-                                    type="text" 
-                                    name="title" 
-                                    label="Task Title" 
-                                    onChange={handleChange} 
-                                    value={title}
-                                />
-                            </Grid>
-
-                            <Grid item xs={4}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined" 
-                                    type="time" 
-                                    name="time-frame" 
-                                    onChange={handleChange} 
-                                    value={timeFrame} 
-                                />
-                            </Grid>
-                            
-                            <Grid item xs={12}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined" 
-                                    multiline 
-                                    name="description" 
-                                    placeholder="Task Description" 
-                                    onChange={handleChange} 
-                                    value={description}
-                                />
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <Button fullWidth type="submit" variant="contained" color="primary">Add</Button>
-                            </Grid>
-                            
-                            
-                        </Grid>
-                    </form>
-                </Container>
-            </Collapse>
-        </div>
-    )
+                color="primary"
+              >
+                Add
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
+    </div>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,79 +1,105 @@
-import React, { useState } from 'react'
-import { auth } from '../firebase/config'
+import React, { useState } from "react";
+import { auth } from "../firebase/config";
 
-import TaskForm from '../components/TaskForm'
-import { Message, Task } from '../types'
-import TaskList from '../components/TaskList'
-import { Box, Button, Fab, makeStyles } from '@material-ui/core'
-import { ListAlt } from '@material-ui/icons'
-import TaskReserve from '../components/TaskReserve'
+import TaskForm from "../components/TaskForm";
+import { Message, Task } from "../types";
+import TaskList from "../components/TaskList";
+import { Box, Button, Fab, Grid, makeStyles } from "@material-ui/core";
+import { ListAlt } from "@material-ui/icons";
+import TaskReserve from "../components/TaskReserve";
 
-import logoWithText from '../images/logo-with-text.png';
-import { NavLink } from 'react-router-dom'
+import logoWithText from "../images/logo-with-text.png";
+import { NavLink } from "react-router-dom";
 
 interface Props {
-    tasks: Task[];
-    reserveTasks: Task[];
-    setMessage: React.Dispatch<React.SetStateAction<Message | null>>;
+  tasks: Task[];
+  reserveTasks: Task[];
+  setMessage: React.Dispatch<React.SetStateAction<Message | null>>;
 }
 
-const useStyles = makeStyles(theme => ({
-    taskReserveButton: {
-        position: 'fixed',
-        bottom: 15,
-        right: 15
-    },
+const useStyles = makeStyles((theme) => ({
+  taskReserveButton: {
+    position: "fixed",
+    bottom: 15,
+    right: 15,
+  },
 
-    logoWithText: {
-        maxWidth: '100%',
-        width: 300
-    },
+  logoWithText: {
+    maxWidth: "100%",
+    width: 300,
+  },
 
-    link: {
-        fontWeight: 'bold',
-        '&:hover': {
-            textDecoration: 'none'
-        }
-    }
-}))
+  link: {
+    fontWeight: "bold",
+    "&:hover": {
+      textDecoration: "none",
+    },
+  },
+}));
 
 export default function Home({ tasks, reserveTasks, setMessage }: Props) {
+  const classes = useStyles();
 
-    const classes = useStyles()
+  const [isTaskReserveOpen, setIsTaskReserveOpen] = useState(false);
 
-    const [isTaskReserveOpen, setIsTaskReserveOpen] = useState(false)
-
-    return (
+  return (
+    <div>
+      {auth.currentUser ? (
         <div>
-            { auth.currentUser ?
-                <div>
-                    <TaskForm user={auth.currentUser}/>
-                    <TaskList tasks={tasks}/>
-                    <TaskReserve 
-                        reserveTasks={reserveTasks} 
-                        open={isTaskReserveOpen} 
-                        user={auth.currentUser} 
-                        setMessage={setMessage}
-                        setIsTaskReserveOpen={setIsTaskReserveOpen}
-                    />
-                    <Fab 
-                        color="primary" 
-                        aria-label="add" 
-                        title="Task Reserve" 
-                        className={classes.taskReserveButton}
-                        onClick={() => setIsTaskReserveOpen(state => !state)}
-                    >
-                        <ListAlt/>
-                    </Fab>
-                </div>
-                :
-                <Box m={2}>
-                    <img src={logoWithText} alt="logo with text" className={classes.logoWithText}/>
-                    <h2>
-                        <Button variant="contained" color="primary"><NavLink to={process.env.PUBLIC_URL + "/signup"} className={classes.link}>Sign Up</NavLink></Button> or <Button variant="contained" color="primary"><NavLink to={process.env.PUBLIC_URL + "/signin"} className={classes.link}>Sign In</NavLink></Button>
-                    </h2>
-                </Box>
-            }
+          <Grid container justify="center">
+            <Grid item md={5}>
+              <TaskForm user={auth.currentUser} />
+            </Grid>
+            <Grid item md={7}>
+              <TaskList tasks={tasks} />
+            </Grid>
+          </Grid>
+
+          <TaskReserve
+            reserveTasks={reserveTasks}
+            open={isTaskReserveOpen}
+            user={auth.currentUser}
+            setMessage={setMessage}
+            setIsTaskReserveOpen={setIsTaskReserveOpen}
+          />
+          <Fab
+            color="primary"
+            aria-label="add"
+            title="Task Reserve"
+            className={classes.taskReserveButton}
+            onClick={() => setIsTaskReserveOpen((state) => !state)}
+          >
+            <ListAlt />
+          </Fab>
         </div>
-    )
+      ) : (
+        <Box m={2}>
+          <img
+            src={logoWithText}
+            alt="logo with text"
+            className={classes.logoWithText}
+          />
+          <h2>
+            <Button variant="contained" color="primary">
+              <NavLink
+                to={process.env.PUBLIC_URL + "/signup"}
+                className={classes.link}
+              >
+                Sign Up
+              </NavLink>
+            </Button>{" "}
+            or{" "}
+            <Button variant="contained" color="primary">
+              <NavLink
+                to={process.env.PUBLIC_URL + "/signin"}
+                className={classes.link}
+              >
+                Sign In
+              </NavLink>
+            </Button>
+          </h2>
+        </Box>
+      )}
+    </div>
+  );
 }
