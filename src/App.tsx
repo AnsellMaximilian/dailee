74b5521e11b02554903785e67ed13921625cb0ce
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-07-04 18:35:45 +0700

    unsubscription for firebase listeners

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,118 @@
-import React, {useEffect, useState} from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import { ThemeProvider } from '@material-ui/styles';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { ThemeProvider } from "@material-ui/styles";
+import firebase from "firebase";
 
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Home from './pages/Home';
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 
-import Header from './components/Header';
+import Header from "./components/Header";
 
-import './App.css';
-import { auth, firestore } from './firebase/config';
-import { Message as MessageType, Task } from './types';
-import theme from './theme';
-import Message from './components/Message';
+import "./App.css";
+import { auth, firestore } from "./firebase/config";
+import { Message as MessageType, Task } from "./types";
+import theme from "./theme";
+import Message from "./components/Message";
 
 const App = () => {
-
-  const [user, setUser] = useState(auth.currentUser)
-  const [tasks, setTasks] = useState<Task[]>([])
-  const [reserveTasks, setReserveTasks] = useState<Task[]>([])
+  const [user, setUser] = useState(auth.currentUser);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [reserveTasks, setReserveTasks] = useState<Task[]>([]);
   const [message, setMessage] = useState<MessageType | null>(null);
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    const listener = auth.onAuthStateChanged((user) => {
       setUser(user);
-    })
-  }, [])
+    });
+
+    return () => listener();
+  }, []);
 
   useEffect(() => {
-    if(user){
-      firestore.collection('tasks')
-        .where('user', '==', user.uid)
+    let tasksListener: firebase.Unsubscribe | null;
+    let reserveTasksListener: firebase.Unsubscribe | null;
+    if (user) {
+      tasksListener = firestore
+        .collection("tasks")
+        .where("user", "==", user.uid)
         .onSnapshot((snapshot) => {
-          let tasks: Task[] = snapshot.docs.map(docSnapshot => {
+          let tasks: Task[] = snapshot.docs.map((docSnapshot) => {
             let task: Task = {
               id: docSnapshot.id,
               user: user.uid,
-              title: 'Unavailable',
-              description: 'Unavailable',
-              timeFrame: 'Unavailable',
+              title: "Unavailable",
+              description: "Unavailable",
+              timeFrame: "Unavailable",
               ...docSnapshot.data(),
             };
             return task;
-          })
+          });
           setTasks(tasks);
-        })
+        });
 
-      firestore.collection('reserveTasks')
-        .where('user', '==', user.uid)
+      reserveTasksListener = firestore
+        .collection("reserveTasks")
+        .where("user", "==", user.uid)
         .onSnapshot((snapshot) => {
-          let reserveTasks: Task[] = snapshot.docs.map(docSnapshot => {
+          let reserveTasks: Task[] = snapshot.docs.map((docSnapshot) => {
             let reserveTask: Task = {
               id: docSnapshot.id,
               user: user.uid,
-              title: 'Unavailable',
-              description: 'Unavailable',
-              timeFrame: 'Unavailable',
+              title: "Unavailable",
+              description: "Unavailable",
+              timeFrame: "Unavailable",
               ...docSnapshot.data(),
             };
             return reserveTask;
-          })
+          });
           setReserveTasks(reserveTasks);
-        })
+        });
     }
-  }, [user])
+    return () => {
+      tasksListener && tasksListener();
+      reserveTasksListener && reserveTasksListener();
+    };
+  }, [user]);
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Router>
-          <Header/>
+          <Header />
 
           <Switch>
             <Route exact path={process.env.PUBLIC_URL + "/"}>
-              <Home 
-                tasks={tasks} 
+              <Home
+                tasks={tasks}
                 reserveTasks={reserveTasks}
                 setMessage={setMessage}
               />
             </Route>
 
             <Route exact path={process.env.PUBLIC_URL + "/signup"}>
-              { user ? <Redirect to={process.env.PUBLIC_URL + "/"}/> : <SignUp setMessage={setMessage}/>}
+              {user ? (
+                <Redirect to={process.env.PUBLIC_URL + "/"} />
+              ) : (
+                <SignUp setMessage={setMessage} />
+              )}
             </Route>
 
             <Route exact path={process.env.PUBLIC_URL + "/signin"}>
-              { user ? <Redirect to={process.env.PUBLIC_URL + "/"}/> : <SignIn setMessage={setMessage}/>}
-            </Route> 
+              {user ? (
+                <Redirect to={process.env.PUBLIC_URL + "/"} />
+              ) : (
+                <SignIn setMessage={setMessage} />
+              )}
+            </Route>
           </Switch>
-          <Message 
-            open={!!message} 
+          <Message
+            open={!!message}
             message={message}
             handleClose={() => {
               setMessage(null);
@@ -99,6 +122,6 @@ const App = () => {
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
